8f9cde7b68e5e46289f61b76c0a84b74754e2ead
Philippe Rivière <fil@rezo.net>
2019-09-06 23:18:20 +0200

    an associative list rather than a regexp
    (ideally the list would be created by yarn data)

--- src/main.js
+++ src/main.js
@@ -2,14 +2,15 @@
 import notebook from '_notebook';
 
 // intercept and reroute calls
+const fetchAlias = {
+  "https://gist.githubusercontent.com/Fil/6bc12c535edc3602813a6ef2d1c73891/raw/3ae88bf307e740ddc020303ea95d7d2ecdec0d19/points.json": "./points.json",
+  "https://unpkg.com/visionscarto-world-atlas/world/110m_land.geojson": "./110m_land.geojson"
+};
 const _fetch = fetch;
 fetch = function() {
   const a = arguments;
-  switch(a[0]) {
-      case "https://gist.githubusercontent.com/Fil/6bc12c535edc3602813a6ef2d1c73891/raw/3ae88bf307e740ddc020303ea95d7d2ecdec0d19/points.json":
-      case "https://unpkg.com/visionscarto-world-atlas/world/110m_land.geojson":
-        a[0] = "./" + a[0].match(/[^/]*$/)[0];
-  }
+  if (a[0] in fetchAlias)
+    a[0] = fetchAlias[a[0]];
   return _fetch(...a);
 }
 
